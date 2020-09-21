b39d2c9690dd4fbf8745b48767ba4ad58922de97
Dario Carlino <baires82@gmail.com>
2020-09-21 00:36:17 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -19,39 +19,21 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets.chomp
 
-      case input
-      when 'list songs'
-        self.list_songs
-      when 'list artists'
-        self.list_artists
-      when 'list genres'
-        self.list_genres
-      when 'list artists'
-        self.list_artists
-      when 'list artist'
-        self.list_songs_by_artist
-      when 'list genre'
-        self.list_songs_by_genre
-      when 'play song'
-        self.play_song
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
       else
-        "Type in a valid request please"
+        puts
       end
-      # if input == "list songs"
-      #   list_songs
-      # elsif input == "list artists"
-      #   list_artists
-      # elsif input == "list genres"
-      #   list_genres
-      # elsif input == "list artist"
-      #   list_songs_by_artist
-      # elsif input == "list genre"
-      #   list_songs_by_genre
-      # elsif input == "play song"
-      #   play_song
-      # else
-      #   puts
-      # end
     end
   end
 
