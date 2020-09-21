ac4515f1929f18e1cb2eede5dd2d67ae1f0bcf49
Dario Carlino <baires82@gmail.com>
2020-09-21 00:08:04 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -69,7 +69,7 @@ class MusicLibraryController
     input = gets.chomp.to_i
     if input >= 1 && input <= Song.all.length
       list = Song.all.sort {|a, b| a.name <=> b.name}
-      song = list[input + 1]
+      song = list
       binding.pry
       puts "Playing #{song.name} by #{song.artist.name}"
     end
